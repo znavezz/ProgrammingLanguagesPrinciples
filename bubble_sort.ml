78f8796f76f1741c1cbcc833c28d3c4d2e953cff
znavezz <znavez@gmail.com>
2024-12-17 22:58:17 +0200

    changes to fit to the requirements

--- bubble_sort.ml
+++ bubble_sort.ml
@@ -1,21 +1,9 @@
-open Ast_sol
-open Nos_sol
-open Semantics_sol
+open Ast
+open Semantics
 
 (* Helper function to generate variable names x1, x2, ..., xn *)
 let generate_vars n =
   List.init n (fun i -> "x" ^ string_of_int (i + 1))
-
-(* Function to generate statements that initialize variables *)
-let initialize_vars vars values =
-  List.fold_left2
-    (fun acc var value -> Comp (acc, Ass (var, Num value)))
-    Skip vars values
-
-(* Function to generate print statements for variables *)
-let print_vars vars =
-  List.fold_left
-    (fun acc var -> Comp (acc, Ass ("_", Var var))) Skip vars
 
 (* Function to generate bubble sort logic *)
 let generate_bubble_sort vars =
@@ -30,11 +18,18 @@ let generate_bubble_sort vars =
           let var2 = List.nth vars (j + 1) in
           let swap_condition = Gte (Var var1, Var var2) in
           let swap_statement =
-            Comp(Ass ("tmp", Var var1),
-                 Comp(Ass (var1, Var var2),
-                      Ass (var2, Var "tmp"))) in
+            Comp (Ass ("tmp", Var var1),
+                  Comp (Ass (var1, Var var2),
+                        Ass (var2, Var "tmp")))
+          in
           inner_loop (j + 1) (Comp (acc_inner, If (swap_condition, swap_statement, Skip)))
       in
       outer_loop (i + 1) (inner_loop 0 acc)
   in
   outer_loop 0 Skip
+
+(* Main function to generate bubble sort statement for n variables *)
+let run_bubble_sort n =
+  let vars = generate_vars n in
+  let bubble_sort_stm = generate_bubble_sort vars in
+  bubble_sort_stm
--- nos.ml
+++ nos.ml
@@ -1,5 +1,6 @@
 open Ast
 open Semantics
+open Bubble_sort  (* Import the bubble sort logic *)
 
 (* nos function: interpreter for stm *)
 let rec nos (stm, state) = match stm with
@@ -13,10 +14,11 @@ let rec nos (stm, state) = match stm with
   | Repeat (s, b) -> 
       let state' = nos (s, state) in
       if solve_b b state' = "tt" then state'
-      else nos (Repeat (s, b), state');;
+      else nos (Repeat (s, b), state')
 
-(* Tests *)
+(* Combined Tests *)
 let () =
+  (* Existing Tests *)
   print_string "x = ";
   print_int (let new_state = nos (test1, s0) in new_state "x");
   print_endline "";
@@ -57,31 +59,7 @@ let () =
   print_int (let new_state = nos (test_shr, s0) in new_state "x");
   print_endline "";
 
-  print_string "x = ";
-  print_int (let new_state = nos (test_shl_zero, s0) in new_state "x");
-  print_endline "";
-
-  print_string "x = ";
-  print_int (let new_state = nos (test_shr_zero, s0) in new_state "x");
-  print_endline "";
-
-  print_string "x = ";
-  print_int (let new_state = nos (test_shl_negative, s0) in new_state "x");
-  print_endline "";
-
-  print_string "x = ";
-  print_int (let new_state = nos (test_shr_negative, s0) in new_state "x");
-  print_endline "";
-
-  print_string "x = ";
-  print_int (let new_state = nos (test_shl_large, s0) in new_state "x");
-  print_endline "";
-
-  print_string "x = ";
-  print_int (let new_state = nos (test_shr_large, s0) in new_state "x");
-  print_endline "";
-
-  (* Test Repeat 1: Increment x until x >= 5 *)
+  (* Additional Tests for Bit-shifting and Repeat *)
   print_endline "Test Repeat 1: Increment x until x >= 5";
   print_string "x = ";
   print_int (let new_state = nos (test_repeat1, s0) in new_state "x");
@@ -95,24 +73,10 @@ let () =
   print_int (let new_state = nos (test_repeat2, update "y" (Num 1) s0) in new_state "y");
   print_endline "";
 
-
-  (* Test Repeat 3: Decrement a and increment b until a = 0 *)
-  print_endline "Test Repeat 3: Decrement a and increment b until a = 0";
-
-  print_string "a = ";
-  print_int (let new_state = nos (test_repeat3, s0) in new_state "a");
-  print_endline "";
-
-  print_string "b = ";
-  print_int (let new_state = nos (test_repeat3, s0) in new_state "b");
-  print_endline "";
-
- (* Bubble Sort Test *)
-  print_endline "\n--- Bubble Sort Test for n = 5 ---";
-
-  (* Define Variables and Values *)
-  let vars = ["x1"; "x2"; "x3"; "x4"; "x5"] in
-  let values = [3; 5; 1; -2; 100] in
+  (* Bubble Sort Test for n = 5 *)
+  let n = 5 in  (* Define n *)
+  let vars = List.init n (fun i -> "x" ^ string_of_int (i + 1)) in
+  let values = [3; 5; 1; -2; 100] in  (* Initial values for variables *)
 
   (* Initialize State *)
   let initial_state =
@@ -121,34 +85,12 @@ let () =
   in
 
   (* Print Initial State *)
+  print_endline "\n--- Bubble Sort Test for n = 5 ---";
   print_endline "Initial State:";
   List.iter (fun var -> Printf.printf "%s = %d\n" var (initial_state var)) vars;
 
   (* Generate Bubble Sort Statement *)
-  let rec generate_bubble_sort vars =
-    let n = List.length vars in
-    let rec outer_loop i acc =
-      if i >= n then acc
-      else
-        let rec inner_loop j acc_inner =
-          if j >= n - i - 1 then acc_inner
-          else
-            let var1 = List.nth vars j in
-            let var2 = List.nth vars (j + 1) in
-            let swap_condition = Gte (Var var1, Var var2) in
-            let swap_statement =
-              Comp (Ass ("tmp", Var var1),
-                    Comp (Ass (var1, Var var2),
-                          Ass (var2, Var "tmp")))
-            in
-            inner_loop (j + 1) (Comp (acc_inner, If (swap_condition, swap_statement, Skip)))
-        in
-        outer_loop (i + 1) (inner_loop 0 acc)
-    in
-    outer_loop 0 Skip
-  in
-
-  let bubble_sort_stm = generate_bubble_sort vars in
+  let bubble_sort_stm = run_bubble_sort n in
 
   (* Run Bubble Sort *)
   let final_state = nos (bubble_sort_stm, initial_state) in
